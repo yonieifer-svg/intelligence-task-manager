c56ac953231076726acb804e4be660d442a6919f
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-18 12:14:54 +0300

    Mission routes

---
+++ routes/mission_routes.py
@@ -0,0 +1,128 @@
+from fastapi import APIRouter, HTTPException
+import logging
+from database.db_connection import db
+from database.mission_db import MissionDB
+from routes.agents_routes import agents, get_agent_by_id
+from models import Mission
+
+
+missions = MissionDB(db, "missions")
+
+router = APIRouter(prefix="/missions", tags=["missions"])
+
+logger = logging.getLogger(__name__)
+
+
+@router.post("", status_code=201)
+def create_mission(data: Mission):
+
+    if not 1 <= data.difficulty <= 10:
+        raise HTTPException(400, "difficulty between 1 and 10")
+    
+    if not 1 <= data.importance <= 10:
+        raise HTTPException(400, "importance between 1 and 10")
+    
+    missions.create_mission(data.model_dump())
+    return {"message": "Mission Created"}
+
+
+@router.get("")
+def get_all_missions():
+    return missions.get_all_missions()
+
+
+@router.get("/{id}")
+def get_mission_by_id(id: int):
+    mission = missions.get_mission_by_id(id)
+
+    if not mission:
+        raise HTTPException(404, "mission not exists")
+    
+    return mission
+
+
+@router.put("/{id}/assign/{agent_id}")
+def assign_mission(id: int, agent_id: int):
+
+    agent = get_agent_by_id(agent_id)
+    mission = get_mission_by_id(id)
+
+    if mission["status"] != "NEW":
+        raise HTTPException(400, "not NEW mission")
+    
+    if not agent["is_active"]:
+        raise HTTPException(400, "non nactive agent")
+    
+    if len(missions.get_open_missions_by_agent(agent_id)) >= 3:
+        raise HTTPException(400, "3 missions are already open")
+    
+    if mission["risk_level"] == "CRITICAL" and agent["agent_rank"] != "Commander":
+        raise HTTPException(400, "only Commander can take CRITICAL")
+    
+    is_updated = missions.assign_mission(id, agent_id)
+    if is_updated:
+        return {"message": "Updated successfully"}
+
+
+@router.put("/{id}/start")
+def start_mission(id: int):
+    mission = get_mission_by_id(id)
+
+    if mission["status"] != "ASSIGNED":
+            raise HTTPException(400, "only ASSIGNED can be IN_PROGRESS")
+    
+    is_updated = missions.update_mission_status(id, "IN_PROGRESS")
+    if is_updated:
+        return {"message": "Updated successfully"}
+    
+
+@router.put("/{id}/complete")
+def complete_mission(id: int):
+    mission = get_mission_by_id(id)
+
+    if mission["status"] != "IN_PROGRESS":
+            raise HTTPException(400, "can COMPLETED only IN_PROGRESS")
+    
+    is_updated = missions.update_mission_status(id, "COMPLETE")
+    if is_updated:
+        agents.increment_completed(id)
+        return {"message": "Updated successfully"}
+    
+
+@router.put("/{id}/fail")
+def fail_mission(id: int):
+    mission = get_mission_by_id(id)
+
+    if mission["status"] != "IN_PROGRESS":
+            raise HTTPException(400, "can FAILED only IN_PROGRESS")
+    
+    is_updated = missions.update_mission_status(id, "FAILED")
+    if is_updated:
+        agents.increment_failed(id)
+        return {"message": "Updated successfully"}
+
+
+@router.put("/{id}/cancel")
+def cancel_mission(id: int):
+    mission = get_mission_by_id(id)
+
+    if mission["status"] not in ["NEW", "ASSIGNED"] :
+        raise HTTPException(400, "can not be CANCELLED")
+    
+    is_updated = missions.update_mission_status(id, "CANCELLED")
+    if is_updated:
+        return {"message": "Updated successfully"}
+    
+    
+    
+
+
+
+     
+
+
+
+
+
+
+
